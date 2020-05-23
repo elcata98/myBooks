dd2580b08a164114bf83c40f14ebdda7e3379a2d
Francisco Martinez Posadas <elcata98@gmail.com>
2020-05-23 18:10:40 +0900

    books service skeleton

---
+++ BooksService/src/main/kotlin/org/elcata98/mybooks/booksservice/BooksServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.elcata98.mybooks.booksservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BooksServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BooksServiceApplication>(*args)
+}
---
+++ BooksService/src/test/kotlin/org/elcata98/mybooks/booksservice/BooksServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.elcata98.mybooks.booksservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BooksServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
